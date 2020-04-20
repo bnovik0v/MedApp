fdc29712c389f2c9224fb5066ecd79ac1a205978
Borislav Novikov <nvikv-brja@yandex.ru>
2020-04-21 01:36:57 +0300

    bll started

---
+++ MedApp.BLL/CaseReportService.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using MedApp.Core;
+using MedApp.Core.Models;
+using MedApp.Core.Services;
+
+namespace MedApp.BLL
+{
+    public class CaseReportService : ICaseReportService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public CaseReportService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<CaseReport> CreateCaseReport(CaseReport newCaseReport)
+        {
+            if (newCaseReport is null)
+                throw new NullReferenceException();
+
+            await _unitOfWork.CaseReports.AddAsync(newCaseReport);
+            await _unitOfWork.CommitAsync();
+
+            return newCaseReport;
+        }
+
+        public async Task<CaseReport> GetCaseReportById(int id)
+        {
+            return await _unitOfWork.CaseReports.GetWithPatientByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<CaseReport>> GetCaseReportsByPatientId(int artistId)
+        {
+            return await _unitOfWork.CaseReports.GetAllWithPatientByPatientIdAsync(artistId);
+        }
+
+        public async Task<IEnumerable<CaseReport>> GetAllWithPatient()
+        {
+            return await _unitOfWork.CaseReports.GetAllWithPatientAsync();
+        }
+
+        public async Task UpdateCaseReport(int id, CaseReport caseReport)
+        {
+            if (!await _unitOfWork.CaseReports.IsExists(id))
+                throw new NullReferenceException();
+
+            if (caseReport.Diagnosis.Length <= 0 || caseReport.Diagnosis.Length > 50 || caseReport.PatientId <= 0)
+                throw new InvalidDataException();
+
+            var caseReportToBeUpdated = await GetCaseReportById(id);
+            caseReportToBeUpdated.Diagnosis = caseReport.Diagnosis;
+            caseReportToBeUpdated.PatientId = caseReport.PatientId;
+
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeleteCaseReport(CaseReport caseReport)
+        {
+            if (!(await _unitOfWork.CaseReports.IsExists(caseReport.Id)))
+                throw new NullReferenceException();
+
+            _unitOfWork.CaseReports.Remove(caseReport);
+
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
---
+++ MedApp.BLL/PatientService.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Threading.Tasks;
+using MedApp.Core;
+using MedApp.Core.Models;
+using MedApp.Core.Services;
+
+namespace MedApp.BLL
+{
+    public class PatientService : IPatientService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+
+        public PatientService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public async Task<Patient> CreatePatient(Patient newPatient)
+        {
+            if (newPatient is null)
+                throw new NullReferenceException();
+
+            await _unitOfWork.Patients.AddAsync(newPatient);
+            await _unitOfWork.CommitAsync();
+
+            return newPatient;
+        }
+
+        public async Task<Patient> GetPatientById(int id)
+        {
+            return await _unitOfWork.Patients.GetByIdAsync(id);
+        }
+
+        public async Task<IEnumerable<Patient>> GetAllPatients()
+        {
+            return await _unitOfWork.Patients.GetAllAsync();
+        }
+
+        public async Task UpdatePatient(int id, Patient patient)
+        {
+            if (!await _unitOfWork.Patients.IsExists(id))
+                throw new NullReferenceException();
+
+            if (patient.FullName.Length == 0 || patient.FullName.Length > 50)
+                throw new InvalidDataException();
+
+            var patientToBeUpdated = await GetPatientById(id);
+            patientToBeUpdated.FullName = patient.FullName;
+
+            await _unitOfWork.CommitAsync();
+        }
+
+        public async Task DeletePatient(Patient patient)
+        {
+            if (!await _unitOfWork.Patients.IsExists(patient.Id))
+                throw new NullReferenceException();
+
+            _unitOfWork.Patients.Remove(patient);
+
+            await _unitOfWork.CommitAsync();
+        }
+    }
+}
