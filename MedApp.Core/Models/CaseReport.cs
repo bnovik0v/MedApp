b2039b07d69c546855776a080c36d2acb0d6a51f
Borislav Novikov <nvikv-brja@yandex.ru>
2020-04-21 00:20:41 +0300

    casereport upload

---
+++ MedApp.Core/Models/CaseReport.cs
@@ -0,0 +1,11 @@
+﻿
+namespace MedApp.Core.Models
+{
+    class CaseReport
+    {
+        public int Id { get; set; }
+        public int PatientId { get; set; }
+        public string Description { get; set; }
+        public string Diagnosis { get; set; }
+    }
+}
