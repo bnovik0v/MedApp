fd0c9dd4a34645d1e8b6e66ad7946e71854757c0
Borislav Novikov <nvikv-brja@yandex.ru>
2020-04-21 20:12:14 +0300

    add migration

---
+++ MedApp.Dal/Migrations/20200421165802_SeedCaseReportsAndPatientsTable.cs
@@ -0,0 +1,65 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MedApp.DAL.Migrations
+{
+    public partial class SeedCaseReportsAndPatientsTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("INSERT INTO Patients (FullName) Values ('Kupriyanov')");
+
+            migrationBuilder.Sql("INSERT INTO Patients (FullName) Values ('Solnzev')");
+
+            migrationBuilder.Sql("INSERT INTO Patients (FullName) Values ('Gorohov')");
+
+            migrationBuilder.Sql("INSERT INTO Patients (FullName) Values ('Surkov')");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('rhinitis', (SELECT Id FROM Patients WHERE Name = 'Kupriyanov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('cancer', (SELECT Id FROM Patients WHERE Name = 'Kupriyanov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('anaemia', (SELECT Id FROM Patients WHERE Name = 'Kupriyanov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('migraine', (SELECT Id FROM Patients WHERE Name = 'Kupriyanov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('hypertension', (SELECT Id FROM Patients WHERE Name = 'Kupriyanov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('AIDS', (SELECT Id FROM Patients WHERE Name = 'Solnzev'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('asthma', (SELECT Id FROM Patients WHERE Name = 'Solnzev'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('allergy', (SELECT Id FROM Patients WHERE Name = 'Solnzev'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('leukemia', (SELECT Id FROM Patients WHERE Name = 'Solnzev'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('hepatitis', (SELECT Id FROM Patients WHERE Name = 'Solnzev'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('jaundice', (SELECT Id FROM Patients WHERE Name = 'Gorohov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('diphtheria', (SELECT Id FROM Patients WHERE Name = 'Gorohov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('herpes', (SELECT Id FROM Patients WHERE Name = 'Gorohov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('haemorrhoids', (SELECT Id FROM Patients WHERE Name = 'Gorohov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('deafness', (SELECT Id FROM Patients WHERE Name = 'Gorohov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('alopecia', (SELECT Id FROM Patients WHERE Name = 'Surkov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('dementia', (SELECT Id FROM Patients WHERE Name = 'Surkov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('conjunctivitis', (SELECT Id FROM Patients WHERE Name = 'Surkov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('typhoid', (SELECT Id FROM Patients WHERE Name = 'Surkov'))");
+
+            migrationBuilder.Sql("INSERT INTO CaseReports (Diagnosis, PatientId) Values ('plague', (SELECT Id FROM Patients WHERE Name = 'Surkov'))");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.Sql("DELETE FROM CaseReports");
+
+            migrationBuilder.Sql("DELETE FROM Patients");
+        }
+    }
+}
